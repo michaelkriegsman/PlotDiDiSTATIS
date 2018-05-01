cd4b24abaf56c85c3ee0793a31cc81be2e1ed396
Michael A. Kriegsman <kriegsman.1@gmail.com>
2018-05-01 08:53:32 -0500

    Updated PlotConfusion to handle percentages

--- R/PlotConfusion.R
+++ R/PlotConfusion.R
@@ -5,20 +5,23 @@
 #'@return A pretty confusion matrix
 #'@export
 
-PlotConfusion <- function(Confusion_mat = NULL){
+PlotConfusion <- function(Confusion_mat = NULL, is.percent = FALSE){
 
-  if(max(Confusion_mat)<1){
-    scale_max <- 1
-  }else{
+  if(is.percent==FALSE){
     scale_max <- ceiling(max(Confusion_mat))
   }
+  if(is.percent==TRUE){
+    scale_max <- 100
+  }
 
-  dev.new()
   corrplot(Confusion_mat,
-           method = "square",
+           method = "square", #circle, square, pie, shade, color
            col=viridis(40, direction=1, option="plasma",
-                       begin = 0, end=.94, alpha = 1),
-           cl.lim = c(0,scale_max), cl.length=11,
+                       begin = 0, end=1, alpha = 1),
+           cl.lim = c(0, scale_max), cl.length=11,
+           addCoef.col = "black", addCoefasPercent = F,
+           tl.cex = 1.25, cl.cex = 1, number.cex = 1.25,
            is.corr = F)
+
 }
 
