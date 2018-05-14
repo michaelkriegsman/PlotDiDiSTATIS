e4c0814b82721c81be7500de673c1bf83d193e27
Michael A. Kriegsman <kriegsman.1@gmail.com>
2018-05-14 16:08:39 -0500

    Plot Confusion update

--- R/PlotConfusion.R
+++ R/PlotConfusion.R
@@ -17,11 +17,18 @@ PlotConfusion <- function(Confusion_mat = NULL, is.percent = FALSE){
   corrplot(Confusion_mat,
            method = "square", #circle, square, pie, shade, color
            col=viridis(40, direction=1, option="plasma",
-                       begin = 0, end=1, alpha = 1),
+                       begin = 0, end=.9, alpha = 1),
            cl.lim = c(0, scale_max), cl.length=11,
            addCoef.col = "black", addCoefasPercent = F,
            tl.cex = 1.25, cl.cex = 1, number.cex = 1.25,
            is.corr = F)
+
+
+  ###Plot the Chi2 test, to see which cells are different from expected
+  # Expect <- matrix(100/res_DiMDS$input$DESIGN_rows$B, res_DiMDS$input$DESIGN_rows$B, res_DiMDS$input$DESIGN_rows$B)
+  # Chi2 <- (res_DiMDS$LOO_Rows$Confusion_rand_norm - Expect)^2 / Expect
+  # corrplot(Chi2 * sign(res_DiMDS$LOO_Rows$Confusion_rand_norm - Expect), is.cor = FALSE, addCoef.col = "gray60")
+
 
 }
 
